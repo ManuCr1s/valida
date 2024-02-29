08d1ec351b5b536286e7de161fbc480be8ab6b0e
mcristobaln <mcristobal@outlook.com>
2024-02-29 18:25:58 -0500

    Fix: Add route page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     require_once 'app/controllers/pagesController.php';
     define('VIEW_PATH',__DIR__.'/app/views');
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    echo $uri;
     switch ($uri){
         case '/':
             $controller = new pageControllers();
